c7effd6e62ba9c05b8663e7145270565d7e619bd
JIAQI13 <jiaqi11@ualberta.ca>
2021-05-21 01:18:24 +0000

    day 2 compass tasks applied

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,100 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+// Fake data taken from initial-tweets.json
+
+
+const renderTweets = function (tweets) {
+  for (let tweet of tweets) {
+    $('.tweet-section').prepend(createTweetElement(tweet));
+  }
+}
+
+//https://timeago.org/ sample code
+const locale = function (number, index, totalSec) {
+  return [
+    ['just now', 'right now'],
+    ['%s seconds ago', 'in %s seconds'],
+    ['1 minute ago', 'in 1 minute'],
+    ['%s minutes ago', 'in %s minutes'],
+    ['1 hour ago', 'in 1 hour'],
+    ['%s hours ago', 'in %s hours'],
+    ['1 day ago', 'in 1 day'],
+    ['%s days ago', 'in %s days'],
+    ['1 week ago', 'in 1 week'],
+    ['%s weeks ago', 'in %s weeks'],
+    ['1 month ago', 'in 1 month'],
+    ['%s months ago', 'in %s months'],
+    ['1 year ago', 'in 1 year'],
+    ['%s years ago', 'in %s years']
+  ][index];
+};
+//preventing xss attack with esacaping
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+const createTweetElement = function (tweet) {
+  let $tweet =
+    `
+  <section>
+  <div style='width:100%;' class='tweet-header'>
+    <div>
+      <img class='resize' src=${escape(tweet.user.avatars)} >
+      <label>${escape(tweet.user.name)}</label>
+    </div>
+    <label class='username'>${escape(tweet.user.handle)}</label>
+  </div>
+  <div>
+    <p id='tweet-content'>${escape(tweet.content.text)}</p>
+  </div>
+  <div id='tweet-button'>
+    <label class='time-ago'>${timeago.format(tweet.created_at)}</label>
+    <div>
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+    </div>
+  </div>
+  </div>
+</section>
+
+  `
+  return $tweet;
+}
+
+const loadTweets = () => {
+  $.ajax({
+    url: '/tweets',
+    method: 'GET'
+  })
+    .then((tweets) => {
+      renderTweets(tweets);
+      $('#tweet-form').reset();
+    })
+}
+
+$(function () {
+  $('#tweet-form').on('submit', function (event) {
+    event.preventDefault();
+    if ($('textarea').val().length >140){
+      $('.tooLong').show();
+      return false;
+    }
+    if ($('textarea').val().length === 0){
+      $('.tooShort').show();
+      return false;
+    }
+    $('.tweet-section').empty();
+    $.ajax('/tweets', {
+      method: 'POST',
+      data: $(this).serialize()
+    })
+      .then(() => {
+        loadTweets();
+      });
+  });
+});
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,30 +1,28 @@
-$(document).ready(function() {
-    //initial word count
-    let chars = $('textarea').val().length;
-    $('.counter').html(chars + ' characters');
-
-    $('.tweet-text').on('keydown', function () {
+$(document).ready(function () {
+    $('.tooLong').hide();
+    $('.tooShort').hide();
+    $('.tweet-text').on('keyup', function () {
 
         // count when user input
         let chars = $('textarea').val().length;
-        $('.counter').html(chars + ' characters');
+        $('.counter').html(140-chars);
 
         //color changing based on word count
         if ($('textarea').val().length > 140) {
+            $('.tooLong').show();
             $(".counter").css("color", "red");
             chars = 140 - $('textarea').val().length;
-            $('.counter').html(chars + ' characters');
-        }else{
+            $('.counter').html(chars);
+        } else {
+            $('.tooLong').hide();
+            $('.tooShort').hide();
             $(".counter").css("color", "");
         }
     });
-    /*
-    $('.fas').mousemove(function () {
-        $(".fas").css("color", "red");
-    });
-    $('.fas').mouseout(function () {
-        $(".fas").css("color", "");
-    });
-    */
+
+    
+    
+
+    
 
 });
